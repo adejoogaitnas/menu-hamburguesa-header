ab20ff45ce0bbd8b06b20695533e9fafd1653015
adejo <adejoogaitnas@gmail.com>
2023-11-02 16:53:24 +0100

    primera versión

---
+++ scripts/hamburguesa.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+// function toggleNav() {
+//     navLinks.classList.toggle('active');
+// }
+
+// hamburger.addEventListener('click',toggleNav)
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
